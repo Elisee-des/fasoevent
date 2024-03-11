da1ad0e32e8c12a5f3970d6a4005c0d4d4327ece
Elisee-dev <esabidani@gmail.com>
2024-03-11 12:42:00 +0000

    Implemtation de template promoteur et abonne

--- resources/views/public/_layouts/app.blade.php
+++ resources/views/public/_layouts/app.blade.php
@@ -51,6 +51,9 @@
             <div class="cta d-none d-md-flex align-items-center gap-2">
                 <a href="#about" class="scrollto">Connexion</a>
                 <a href="#about" class="scrollto">Inscription</a>
+                <a href="{{ route('private.admintableaudebord') }}" class="scrollto">Mon compte A</a>
+                <a href="{{ route('promoteur.tableaudebord') }}" class="scrollto">Mon compte P</a>
+                <a href="{{ route('private.abonnetableaudebord') }}" class="scrollto">Mon compte Ab</a>
             </div>
         </div>
     </section>
